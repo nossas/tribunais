6e173e1eda61e43209c1f6f04bdd5416285022e4
Nícolas Iensen <nicolas@iensen.me>
2014-07-16 13:32:59 -0300

    [#74981548] add memberships_attributes to the user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,23 @@ class User < ActiveRecord::Base
   has_many :pokes
 
   def self.create params
+    organization = Organization.find_by_slug('meurio')
+
     if Rails.env.production? || Rails.env.staging?
       begin
         url = "#{ENV["ACCOUNTS_HOST"]}/users.json"
-        user_hash = { first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: SecureRandom.hex, application_slug: "naopassarao", ip: params[:ip] }
+
+        user_hash = {
+          first_name: params[:first_name],
+          last_name: params[:last_name],
+          email: params[:email],
+          password: SecureRandom.hex,
+          ip: params[:ip],
+          memberships_attributes: [{organization_id: organization.id}]
+        }
+
         body = { token: ENV["ACCOUNTS_API_TOKEN"], user: user_hash }
         response = HTTParty.post(url, body: body.to_json, headers: { 'Content-Type' => 'application/json' })
-        logger.info response.inspect
         User.find_by_id(response['id'])
       rescue Exception => e
         logger.error e.message
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140408084725) do
+ActiveRecord::Schema.define(version: 20140716162521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20140408084725) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "organizations", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "slug"
+  end
 
   create_table "pokes", force: true do |t|
     t.string   "ip"
